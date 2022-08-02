32c76f26c97afdb30b6f034852ee6009be154789
Eyüp Fidan <60450646+eyupfidan@users.noreply.github.com>
2022-08-02 21:24:39 +0300

    Add 'Project Layout' Project
    Layoutlarda sınırların (Constraints) manuel olarak düzenlenmesi.

---
+++ ProjectLayout/ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Eyüp Fidan on 8.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
