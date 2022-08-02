c679407e70de070d4cb347ee7e77cf0f417c6fc1
Eyüp Fidan <60450646+eyupfidan@users.noreply.github.com>
2022-08-02 21:43:33 +0300

    Add 'CatchtheKenny' Project
    Using Catch the Kenny game recognizer timer and UI functions together

---
+++ CatchtheKenny/CatchtheKenny/ViewController.swift
@@ -0,0 +1,160 @@
+//
+//  ViewController.swift
+//  CatchtheKenny
+//
+//  Created by Eyüp Fidan on 11.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //Variables
+    var score = 0
+    var highScore = 0
+    var timer = Timer()
+    var hideTimer = Timer()
+    var counter = 0
+    var kennyArray = [UIImageView]()
+    //Labels
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    
+    //Images
+    @IBOutlet weak var kenny1: UIImageView!
+    @IBOutlet weak var kenny2: UIImageView!
+    @IBOutlet weak var kenny3: UIImageView!
+    @IBOutlet weak var kenny4: UIImageView!
+    @IBOutlet weak var kenny5: UIImageView!
+    @IBOutlet weak var kenny6: UIImageView!
+    @IBOutlet weak var kenny7: UIImageView!
+    @IBOutlet weak var kenny8: UIImageView!
+    @IBOutlet weak var kenny9: UIImageView!
+    @IBOutlet weak var kenny10: UIImageView!
+    @IBOutlet weak var kenny11: UIImageView!
+    @IBOutlet weak var kenny12: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scoreLabel.text = "Score: \(score)"
+        //Highscore Check
+        
+        let storedHighScore = UserDefaults.standard.object(forKey: "highscore")
+        
+        if storedHighScore == nil {
+            highScore = 0
+            highscoreLabel.text = "Highscore: \(highScore)"
+        }
+        
+        if let newScore = storedHighScore as? Int {
+            highScore = newScore
+            highscoreLabel.text = "Highscore: \(highScore)"
+        }
+        kenny1.isUserInteractionEnabled = true
+        kenny2.isUserInteractionEnabled = true
+        kenny3.isUserInteractionEnabled = true
+        kenny4.isUserInteractionEnabled = true
+        kenny5.isUserInteractionEnabled = true
+        kenny6.isUserInteractionEnabled = true
+        kenny7.isUserInteractionEnabled = true
+        kenny8.isUserInteractionEnabled = true
+        kenny9.isUserInteractionEnabled = true
+        kenny10.isUserInteractionEnabled = true
+        kenny11.isUserInteractionEnabled = true
+        kenny12.isUserInteractionEnabled = true
+
+        
+        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer10 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer11 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer12 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        
+        kenny1.addGestureRecognizer(recognizer1)
+        kenny2.addGestureRecognizer(recognizer2)
+        kenny3.addGestureRecognizer(recognizer3)
+        kenny4.addGestureRecognizer(recognizer4)
+        kenny5.addGestureRecognizer(recognizer5)
+        kenny6.addGestureRecognizer(recognizer6)
+        kenny7.addGestureRecognizer(recognizer7)
+        kenny8.addGestureRecognizer(recognizer8)
+        kenny9.addGestureRecognizer(recognizer9)
+        kenny10.addGestureRecognizer(recognizer10)
+        kenny11.addGestureRecognizer(recognizer11)
+        kenny12.addGestureRecognizer(recognizer12)
+        
+        kennyArray = [kenny1,kenny2,kenny3,kenny4,kenny5,kenny6,kenny7,kenny8,kenny9,kenny10,kenny11,kenny12]
+
+      //timers
+        counter = 10
+        timeLabel.text = String(counter)
+        timer = Timer.scheduledTimer(timeInterval:1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideKenny), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func hideKenny () {
+        for kenny in kennyArray {
+            kenny.isHidden = true
+        }
+        
+        let random = Int(arc4random_uniform(UInt32(kennyArray.count - 1)))
+        kennyArray[random].isHidden = false
+    }
+    @objc func increaseScore() {
+        score += 1
+        scoreLabel.text = "Score:\(score) "
+    }
+    
+    @objc func countDown() {
+        
+        counter -= 1
+        timeLabel.text = String(counter)
+        if counter == 0 {
+            timer.invalidate()
+            hideTimer.invalidate()
+            
+            for kenny in kennyArray {
+                kenny.isHidden = true
+            }
+            
+            if self.score > self.highScore {
+                self.highScore = self.score
+                highscoreLabel.text = "Highscore: \(self.highScore)"
+                UserDefaults.standard.set(self.highScore, forKey: "highscore")
+            }
+            //Alert
+            let alert = UIAlertController(title:"Time's Up", message: "Do you want to play again?", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            let replayButton = UIAlertAction(title: "Again", style: UIAlertAction.Style.default) { [self]
+                (UIAlertAction) in
+                //replay function
+                self.score = 0
+                self.scoreLabel.text = "Score: \(self.score)"
+                self.counter = 10
+                self.timeLabel.text = String(self.counter)
+                
+                self.timer = Timer.scheduledTimer(timeInterval:1, target: self, selector: #selector(self.countDown), userInfo: nil, repeats: true)
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideKenny), userInfo: nil, repeats: true)
+
+            }
+            
+            
+            
+            alert.addAction(okButton)
+            alert.addAction(replayButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        
+    }
+}
+
---
+++ CatchtheKenny/README.md
@@ -0,0 +1,10 @@
+
+# CatchtheKenny 
+
+Using Catch the Kenny game recognizer timer and UI functions together
+
+
+
+
+
+![CatchtheKenny](https://raw.githubusercontent.com/eyupfidan/CatchtheKenny/main/assets/CatchtheKenny.gif)
